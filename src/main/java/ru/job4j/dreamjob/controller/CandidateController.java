4bb77f34f2a063d51ca736a4df6ceccf97a5d761
fromiva <66008363+fromiva@users.noreply.github.com>
2023-06-05 15:58:50 +0300

    6. Создание, просмотр, редактирование и удаление кандидатов [#504858 # [#504858]]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -2,10 +2,12 @@ package ru.job4j.dreamjob.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.repository.CandidateRepository;
 import ru.job4j.dreamjob.repository.MemoryCandidateRepository;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/candidates")
@@ -18,8 +20,48 @@ public class CandidateController {
         return "candidates/list";
     }
 
+    @GetMapping("/{id}")
+    public String getById(Model model, @PathVariable int id) {
+        Optional<Candidate> candidateOptional = repository.findById(id);
+        if (candidateOptional.isEmpty()) {
+            model.addAttribute("message",
+                    "Кандидат с указанным идентификатором не найден");
+            return "errors/404";
+        }
+        model.addAttribute("candidate", candidateOptional.get());
+        return "candidates/one";
+    }
+
     @GetMapping("/create")
     public String getCreationPage() {
         return "candidates/create";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute Candidate candidate) {
+        repository.save(candidate);
+        return "redirect:/candidates";
+    }
+
+    @PostMapping("/update")
+    public String update(Model model, @ModelAttribute Candidate candidate) {
+        boolean success = repository.update(candidate);
+        if (!success) {
+            model.addAttribute("message",
+                    "Кандидат с указанным идентификатором не найден");
+            return "errors/404";
+        }
+        return "redirect:/candidates";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(Model model, @PathVariable int id) {
+        boolean success = repository.deleteById(id);
+        if (!success) {
+            model.addAttribute("message",
+                    "Кандидат с указанным идентификатором не найден");
+            return "errors/404";
+        }
+        return "redirect:/candidates";
     }
 }
