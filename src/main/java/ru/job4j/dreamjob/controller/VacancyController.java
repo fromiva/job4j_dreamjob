814484e826dde16567e51b19f9993be10b3fcc71
fromiva <66008363+fromiva@users.noreply.github.com>
2023-06-05 15:02:34 +0300

    5. Просмотр, редактирование и удаление вакансии [#504850  [#504850]]

--- src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
+++ src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
@@ -2,13 +2,12 @@ package ru.job4j.dreamjob.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.job4j.dreamjob.model.Vacancy;
 import ru.job4j.dreamjob.repository.MemoryVacancyRepository;
 import ru.job4j.dreamjob.repository.VacancyRepository;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/vacancies")
@@ -21,9 +20,43 @@ public class VacancyController {
         return "vacancies/list";
     }
 
+    @GetMapping("/{id}")
+    public String getById(Model model, @PathVariable int id) {
+        Optional<Vacancy> vacancyOptional = repository.findById(id);
+        if (vacancyOptional.isEmpty()) {
+            model.addAttribute("message",
+                    "Вакансия с указанным идентификатором не найдена");
+            return "errors/404";
+        }
+        model.addAttribute("vacancy", vacancyOptional.get());
+        return "vacancies/one";
+    }
+
     @GetMapping("/create")
     public String getCreationPage() {
         return "vacancies/create";
+    }
+
+    @PostMapping("/update")
+    public String update(Model model, @ModelAttribute Vacancy vacancy) {
+        boolean success = repository.update(vacancy);
+        if (!success) {
+            model.addAttribute("message",
+                    "Вакансия с указанным идентификатором не найдена");
+            return "errors/404";
+        }
+        return "redirect:/vacancies";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(Model model, @PathVariable int id) {
+        boolean success = repository.deleteById(id);
+        if (!success) {
+            model.addAttribute("message",
+                    "Вакансия с указанным идентификатором не найдена");
+            return "errors/404";
+        }
+        return "redirect:/vacancies";
     }
 
     @PostMapping("/create")
